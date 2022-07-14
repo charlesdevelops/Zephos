54bc1c48b98882917f07eeb07d67ddd64417748a
Charles <charlesaidanwatson@icloud.com>
2022-07-14 11:28:38 +0800

    Initial Commit

---
+++ Zephos/ZephosApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZephosApp.swift
+//  Zephos
+//
+//  Created by Charles Watson on 14/7/2022.
+//
+
+import SwiftUI
+
+@main
+struct ZephosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
